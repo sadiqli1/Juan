385e07c25249c845062094decf5d014ef338765d
sadiqli1 <isaas@code.edu.az>
2022-05-24 12:50:23 +0400

    imploment shop page

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,11 +1,11 @@
-window.onload = function (){
+window.onload = function () {
     const description = document.querySelector("#third .wrapper .custom-container .cus-title ul #description");
     const information = document.querySelector("#third .wrapper .custom-container .cus-title ul #information");
     const reviews = document.querySelector("#third .wrapper .custom-container .cus-title ul #reviews");
     const descriptionO = document.querySelector("#third .wrapper .custom-container .description");
     const informationO = document.querySelector("#third .wrapper .custom-container .information");
     const reviewsO = document.querySelector("#third .wrapper .custom-container .reviews");
-    information.addEventListener("click", () =>{
+    information.addEventListener("click", () => {
         informationO.classList.add("activ");
         descriptionO.classList.remove("activ");
         reviewsO.classList.remove("activ");
@@ -13,7 +13,7 @@ window.onload = function (){
         description.classList.remove("activ-links");
         reviews.classList.remove("activ-links");
     });
-    reviews.addEventListener("click", () =>{
+    reviews.addEventListener("click", () => {
         informationO.classList.remove("activ");
         descriptionO.classList.remove("activ");
         reviewsO.classList.add("activ");
@@ -21,7 +21,7 @@ window.onload = function (){
         description.classList.remove("activ-links");
         reviews.classList.add("activ-links");
     });
-    description.addEventListener("click", () =>{
+    description.addEventListener("click", () => {
         informationO.classList.remove("activ");
         descriptionO.classList.add("activ");
         reviewsO.classList.remove("activ");
@@ -48,9 +48,40 @@ window.onload = function (){
         }
     });
 
-    const img = document.querySelector("#second .wrapper .custom-container .row .col-lg-5 .image img");
-    img.addEventListener("mousemove", (e)=>{
-        // console.log(e.target.offsetX)
-        img.style.transform = "scale(1.2)";
-    })
+    const img = document.querySelectorAll("#second .wrapper .custom-container .row .col-lg-5 .images-lists .img img");
+    const imgcon = document.querySelectorAll("#second .wrapper .custom-container .row .col-lg-5 #zoom");
+    for (let i = 0; i < img.length; i++) {
+        img[i].addEventListener("click", () => {
+            imgcon[0].children[0].src = img[i].src;
+            imgcon[0].style.backgroundImage = `url(${img[i].src})`
+        });
+    };
+
+    const minus = document.querySelector("#second .wrapper .custom-container .row .col-lg-7 .product-quantity-box .quantity #minus");
+    const plus = document.querySelector("#second .wrapper .custom-container .row .col-lg-7 .product-quantity-box .quantity #plus");
+    const count = document.querySelector("#second .wrapper .custom-container .row .col-lg-7 .product-quantity-box .quantity #count");
+    let counnum = parseFloat(count.innerHTML);
+    minus.addEventListener("click", () => {
+        if(counnum > 1){
+            counnum -= 1;
+            count.innerHTML = counnum;
+        }
+        else{
+
+        }
+    });
+    plus.addEventListener("click", () => {
+        counnum += 1;
+        console.log(counnum)
+        count.innerHTML = counnum;
+    });
 };
+
+function zoom(e) {
+    var zoomer = e.currentTarget;
+    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+    x = offsetX / zoomer.offsetWidth * 100
+    y = offsetY / zoomer.offsetHeight * 100
+    zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
